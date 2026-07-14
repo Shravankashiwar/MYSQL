558dc972ab0287e3dfbaa3e24deeca1d8995188c
Shravankashiwar <168186723+Shravankashiwar@users.noreply.github.com>
2026-07-14 20:21:28 +0530

    Create 04-South America In millions.sql

---
+++ SQL Zoo-Solutions/Select From World/04-South America In millions.sql
@@ -0,0 +1,5 @@
+-- Show the name and population in millions for the countries of the continent 'South America'.
+-- Divide the population by 1000000 to get population in millions. 
+
+SELECT name, population/1000000 FROM world
+WHERE continent = 'south america';
