2a901f693af776867b7b9f033bde3cfa1ba00496
Shravankashiwar <168186723+Shravankashiwar@users.noreply.github.com>
2026-07-15 19:36:40 +0530

    Update 05-Literature in the 1980's.sql

--- SQL Zoo-Solutions/Select From Nobel/05-Literature in the 1980's.sql
+++ SQL Zoo-Solutions/Select From Nobel/05-Literature in the 1980's.sql
@@ -1,3 +1,5 @@
+-- Show all details (yr, subject, winner) of the literature prize winners for 1980 to 1989 inclusive. 
+
 SELECT * FROM nobel
 WHERE subject = 'literature' AND
 yr BETWEEN 1980 AND 1989;
