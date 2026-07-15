5166d61d97a5e08cbc05ccbce592617040b94dea
Shravankashiwar <168186723+Shravankashiwar@users.noreply.github.com>
2026-07-15 19:34:49 +0530

    Create 08-Chemistry and Physics from different years.sql

---
+++ SQL Zoo-Solutions/Select From Nobel/08-Chemistry and Physics from different years.sql
@@ -0,0 +1,5 @@
+-- Show the year, subject, and name of physics winners for 1980 together with the chemistry winners for 1984.
+
+SELECT * FROM nobel
+WHERE (subject = 'Physics' AND yr = 1980)
+   OR (subject = 'Chemistry' AND yr = 1984);
